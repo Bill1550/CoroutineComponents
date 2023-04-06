e529e63d13d03cf07678bb8ce94fe5c346529c96
BillH <bill.hart@mavenclinic.com>
2023-04-06 12:54:45 -0400

    Added an initial case solution for call back flow using concatentation of flows.

--- coroutine/src/androidTest/java/com/loneoaktech/components/coroutine/basic/BasicThreadSourceTests.kt
+++ coroutine/src/androidTest/java/com/loneoaktech/components/coroutine/basic/BasicThreadSourceTests.kt
@@ -7,26 +7,25 @@ import app.cash.turbine.test
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flattenConcat
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.IllegalStateException
 import kotlin.test.assertEquals
-import kotlin.test.expect
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
@@ -39,13 +38,13 @@ class BasicThreadSourceTests {
     class DelayedCallback<T>(
         val delay: Long,
     ) {
-        private var _listener: ((T)->Unit)? = null  // should be weak reference
+        private var _listener: ((T) -> Unit)? = null  // should be weak reference
 
         private var handler: Handler? = null
-        private val thread = object: Thread("delay-thread") {
+        private val thread = object : Thread("delay-thread") {
             override fun run() {
                 Looper.prepare()
-                handler = Looper.myLooper() ?. let {
+                handler = Looper.myLooper()?.let {
                     object : Handler(it) {}
                 }
 
@@ -54,16 +53,17 @@ class BasicThreadSourceTests {
             }
         }
 
-        fun setListener( listener: ((T)->Unit) ) {
+        fun setListener(listener: ((T) -> Unit)) {
             _listener = listener
         }
 
-        fun sendItem( item: T) {
+        fun sendItem(item: T) {
             println("sending $item")
             handler?.postDelayed(
-                object: Runnable {
+                object : Runnable {
                     override fun run() {
-                        _listener?.invoke( item )
+                        println("calling listener")
+                        _listener?.invoke(item)
                     }
                 },
                 delay
@@ -71,7 +71,7 @@ class BasicThreadSourceTests {
         }
 
         fun close() {
-            handler?.post { Looper.myLooper()?.quitSafely()}
+            handler?.post { Looper.myLooper()?.quitSafely() }
             Thread.sleep(50)
         }
 
@@ -98,15 +98,17 @@ class BasicThreadSourceTests {
 
         val strCallback = DelayedCallback<String>(100)
 
-        val flow = callbackFlow{
+        val flow = callbackFlow {
             println("flow builder started")
             strCallback.setListener { trySend(it) }
             awaitClose { strCallback.close() }
         }
 
-        val job = launch { flow.collect {
-            println("collected: $it")
-        } }
+        val job = launch {
+            flow.collect {
+                println("collected: $it")
+            }
+        }
 
         delay(10)
         strCallback.sendItem("one")
@@ -118,12 +120,15 @@ class BasicThreadSourceTests {
     fun sharedFlowTest() = runBlocking {
         val strCallback = DelayedCallback<String>(100)
 
-        val flow = callbackFlow{
+        val flow = callbackFlow {
             println("inside flow builder")
             strCallback.setListener { trySend(it) }
             awaitClose { strCallback.close() }
-        }.shareIn( CoroutineScope( Job() + Dispatchers.IO ), started = SharingStarted.WhileSubscribed() ).apply {
-            (this as? MutableSharedFlow)?.subscriptionCount?.let{ sc ->
+        }.shareIn(
+            CoroutineScope(Job() + Dispatchers.IO),
+            started = SharingStarted.WhileSubscribed()
+        ).apply {
+            (this as? MutableSharedFlow)?.subscriptionCount?.let { sc ->
                 launch {
                     sc.collect {
                         println("Subscription count=$it")
@@ -133,7 +138,7 @@ class BasicThreadSourceTests {
 
         }
 
-        println( "subscribing to flow")
+        println("subscribing to flow")
 
         val job = launch(Dispatchers.Main) {
             flow.collect {
@@ -168,7 +173,11 @@ class BasicThreadSourceTests {
     fun stateFlowExample1() = runTest {
         val sharingScope = backgroundScope
         // Changing started to SharingStarted.Eagerly doesn't change the behavior of this test.
-        val stateFlow = flowOfEvents.stateIn(sharingScope, started = SharingStarted.WhileSubscribed(), "initial")
+        val stateFlow = flowOfEvents.stateIn(
+            sharingScope,
+            started = SharingStarted.WhileSubscribed(),
+            "initial"
+        )
         println("initial=${stateFlow.value}")
         stateFlow.test {
             val item0 = awaitItem()
@@ -197,5 +206,73 @@ class BasicThreadSourceTests {
 
             expectNoEvents()
         }
+    }
+
+    // needs to be on runBlocking because delays inside callback are hard thread delays
+    @Test
+    fun secondSuspendCallTest() = runBlocking {
+
+            val strCallback = DelayedCallback<String>(100)
+
+            val flow = callbackFlow {
+                println("flow builder started")
+                strCallback.setListener { trySend(it) }
+                awaitClose { strCallback.close() }
+            }
+
+            suspend fun loadDependentData(key: String): String {
+                delay(50)
+                return "$key-dependent"
+            }
+
+            val dependantFlow = flow.map { loadDependentData(it) }
+
+            val job = launch(Dispatchers.Unconfined) {
+                println("collector started")
+                dependantFlow.collect {
+                    println("collected: $it")
+                }
+            }
+
+            delay(10)
+            strCallback.sendItem("one")
+            delay(500)
+            job.cancel()
+        }
+
+    @OptIn(FlowPreview::class)
+    @Test
+    fun concatenatePrimer() = runBlocking {
+
+        val strCallback = DelayedCallback<String>(100)
+
+        val dataFlow = callbackFlow {
+            println("flow builder started")
+            strCallback.setListener { trySend(it) }
+            awaitClose { strCallback.close() }
+        }
+
+        val initialFlow = flow {
+            delay(10)
+            emit("Initial")
+        }
+
+        val flowWithInitial = flowOf( initialFlow, dataFlow).flattenConcat()
+
+        val collected = mutableListOf<String>()
+        val job = launch(Dispatchers.Unconfined) {
+            println("collector started")
+            flowWithInitial.collect {
+                println("collected: $it")
+                collected.add(it)
+            }
+        }
+
+        delay(10)
+        strCallback.sendItem("One")
+        delay(500)
+        job.cancel()
+
+        assertEquals( listOf("Initial", "One"), collected)
     }
 }
